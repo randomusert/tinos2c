a01b1ab3deaef6e7667d42112bed14f1234fc422
randomusert <eetupajamaki@gmail.com>
2026-03-05 09:06:28 +0000

    add mtools to dependencies. hopefully get iso file uploaded by workflow

--- ubuntu-install-deps.sh
+++ ubuntu-install-deps.sh
@@ -3,5 +3,5 @@ set -e
 
 # Update package lists and install dependencies
 sudo apt update
-sudo apt install -y make binutils clang nasm xorriso qemu-system-i386 grub-pc-bin
+sudo apt install -y make binutils clang nasm xorriso qemu-system-i386 grub-pc-bin mtools
 echo "All dependencies have been installed successfully."
